944b06e005b9e21a579c8ea60233b6cdd3b3bb2b
rachidbah <rachidoubah812@gmail.com>
2025-12-24 13:00:50 +0000

    Configure Cloudinary with credentials v1.4.1

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2
 
 // Configuration Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'daice51xy',
+  api_key: process.env.CLOUDINARY_API_KEY || '978342353197896',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'u82IoTiv9DtIVMcWAxDoXITyBSk'
 })
 
 module.exports = cloudinary
